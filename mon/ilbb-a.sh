15a231b928d1eb321910277a6d5930945f452f89
R <repitae@gmail.com>
2022-10-25 00:02:45 +0300

    mon

---
+++ mon/ilbb-a.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+NS=185.179.144.1
+
+while :
+do
+  for var in mb.vtb.ru online.vtb.ru sso-app.vtb.ru
+    do
+      echo
+      echo `date '+%F %T'`
+      echo "NS: $NS"
+      echo "FQDN: $var"
+      echo -n "ANSWER: "
+      dig a @$NS $var +short
+      sleep 0.33
+    done
+  done
+done
