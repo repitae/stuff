a3fe4fa222ca67400ba3d1cbfc1df926bd4b683f
R <repitae@gmail.com>
2024-01-19 16:37:05 +0300

    Update glibc-up.sh

--- work/glibc-up.sh
+++ work/glibc-up.sh
@@ -5,7 +5,9 @@ tar xzf ./glibc-2.32.tar.gz
 cd glibc-2.32
 mkdir build && cd build
 [[ $? -eq 0 ]] && ../configure --prefix=/app/glibc-2.32
-[[ $? -eq 0 ]] && make && make install
+#[[ $? -eq 0 ]] && make all
+[[ $? -eq 0 ]] && make
+[[ $? -eq 0 ]] && make install
 
 cd /app/src/
 mkdir -p /app/envoy129-bin
@@ -14,6 +16,10 @@ wget https://github.com/envoyproxy/envoy/releases/download/v1.29.0/envoy-1.29.0-
 wget https://github.com/envoyproxy/envoy/releases/download/v1.29.0/envoy-contrib-1.29.0-linux-x86_64
 mv envoy-1.29.0-linux-x86_64 envoy-bin
 mv envoy-contrib-1.29.0-linux-x86_64 envoy-con
+
+echo $LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/glibc-2.32/lib
+
 ldd ./envoy-bin
 ldd ./envoy-con
 
@@ -27,4 +33,7 @@ ldd ./envoy-con
 #sudo ln -s /usr/local/lib/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0
 #/lib/x86_64-linux-gnu/libc.so.6
 #/lib/x86_64-linux-gnu/libpthread.so.0
+
+
+
 
