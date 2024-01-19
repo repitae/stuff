17d83e0de7489570812572a0802d89405b904720
R <repitae@gmail.com>
2024-01-19 16:29:38 +0300

    Update glibc-up.sh

--- work/glibc-up.sh
+++ work/glibc-up.sh
@@ -7,18 +7,7 @@ mkdir build && cd build
 ../configure --prefix=/app/glibc-2.32
 make && make install
 
-cp /app/src/glibc-2.32/build/libm-2.32.so /usr/local/lib/
-cp /app/src/glibc-2.32/build/libpyjread-2.32.so /usr/local/lib/
-
-#mv /lib/x86_64-linux-gnu/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6.bak
-#ln -s /usr/local/lib/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6
-#mv /lib/x86_64-linux-gnu/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0.bak
-#ln -s /usr/local/lib/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0
-
-ldd --version
-/lib/x86_64-linux-gnu/libc.so.6
-/lib/x86_64-linux-gnu/libpthread.so.0
-
+cd /app/src/
 mkdir -p /app/envoy129-bin
 cd /app/envoy129-bin
 wget https://github.com/envoyproxy/envoy/releases/download/v1.29.0/envoy-1.29.0-linux-x86_64
@@ -27,3 +16,15 @@ mv envoy-1.29.0-linux-x86_64 envoy-bin
 mv envoy-contrib-1.29.0-linux-x86_64 envoy-con
 ldd ./envoy-bin
 ldd ./envoy-con
+
+
+### CRITICAL ###
+#sudo cp /app/glibc-2.32/lib/libm-2.32.so /usr/local/lib/libm.so.6
+#sudo cp /app/glibc-2.32/lib/libpthread.so.0 /usr/local/lib/libpthread.so.0
+#sudo mv /lib/x86_64-linux-gnu/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6.bak
+#sudo ln -s /usr/local/lib/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6
+#sudo mv /lib/x86_64-linux-gnu/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0.bak
+#sudo ln -s /usr/local/lib/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0
+#/lib/x86_64-linux-gnu/libc.so.6
+#/lib/x86_64-linux-gnu/libpthread.so.0
+
