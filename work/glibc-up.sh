f6308185aa100b129046d03560666b8e72e9a775
R <repitae@gmail.com>
2024-01-19 16:31:54 +0300

    Update glibc-up.sh

--- work/glibc-up.sh
+++ work/glibc-up.sh
@@ -4,8 +4,8 @@ curl -OL https://ftp.gnu.org/gnu/glibc/glibc-2.32.tar.gz
 tar xzf ./glibc-2.32.tar.gz
 cd glibc-2.32
 mkdir build && cd build
-../configure --prefix=/app/glibc-2.32
-make && make install
+[[ $? -eq 0 ]] && ../configure --prefix=/app/glibc-2.32
+[[ $? -eq 0 ]] && make && make install
 
 cd /app/src/
 mkdir -p /app/envoy129-bin
