cef76f3c40885479df54f8a69b02f1be648e0855
R <repitae@gmail.com>
2024-01-19 16:20:34 +0300

    Update glibc-up.sh

--- work/glibc-up.sh
+++ work/glibc-up.sh
@@ -1,4 +1,5 @@
-apt install bison # deps
+cd /app/src/
+sudo apt install bison # deps
 curl -OL https://ftp.gnu.org/gnu/glibc/glibc-2.32.tar.gz
 tar xzf ./glibc-2.32.tar.gz
 cd glibc-2.32
@@ -6,12 +7,23 @@ mkdir build && cd build
 ../configure --prefix=/app/glibc-2.32
 make && make install
 
-cp /app/src/glibc-2.31/build/libm-2.31.so /usr/local/lib/
-cp /app/src/glibc-2.31/build/libpyjread-2.31.so /usr/local/lib/
+cp /app/src/glibc-2.32/build/libm-2.32.so /usr/local/lib/
+cp /app/src/glibc-2.32/build/libpyjread-2.32.so /usr/local/lib/
 
-mv /lib/x86_64-linux-gnu/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6.bak
-ln -s /usr/local/lib/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6
+#mv /lib/x86_64-linux-gnu/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6.bak
+#ln -s /usr/local/lib/libm.so.6 /lib/x86_64-linux-gnu/libm.so.6
+#mv /lib/x86_64-linux-gnu/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0.bak
+#ln -s /usr/local/lib/libpthread.so.0 /lib/x86_64-linux-gnu/libpthread.so.0
 
 ldd --version
 /lib/x86_64-linux-gnu/libc.so.6
 /lib/x86_64-linux-gnu/libpthread.so.0
+
+mkdir -p /app/envoy129-bin
+cd /app/envoy129-bin
+wget https://github.com/envoyproxy/envoy/releases/download/v1.29.0/envoy-1.29.0-linux-x86_64
+wget https://github.com/envoyproxy/envoy/releases/download/v1.29.0/envoy-contrib-1.29.0-linux-x86_64
+mv envoy-1.29.0-linux-x86_64 envoy-bin
+mv envoy-contrib-1.29.0-linux-x86_64 envoy-con
+ldd ./envoy-bin
+ldd ./envoy-con
