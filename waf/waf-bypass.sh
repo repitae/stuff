bd250cb66d853b420411b3d869284fd96b26af87
repitae <repitae@gmail.com>
2025-10-29 19:58:11 +0300

    Update waf-bypass.sh

--- waf/waf-bypass.sh
+++ waf/waf-bypass.sh
@@ -2,4 +2,4 @@ git clone https://github.com/nemesida-waf/waf_bypass.git /opt/waf-bypass/
 sudo  apt install python3-venv
 python3 -m venv /opt/waf-bypass/
 ./bin/python3 -m pip install -r ./requirements.txt
-./bin/python3 /opt/waf-bypass/main.py --host='anketa.test.ru'
+./bin/python3 /opt/waf-bypass/main.py --host='www.example.com'
